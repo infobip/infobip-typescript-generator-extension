e72cf69c9260d327ecb97f4f546c67a1660edc55
lpandzic <lovro.pandzic@infobip.com>
2021-05-07 07:56:27 +0200

    fixed HierarchyTypeScriptFileGeneratorTest.java

--- infobip-typescript-generator-extension-annotation-processor-showcase/src/test/java/com/infobip/typescript/showcase/hierarchy/HierarchyTypeScriptFileGeneratorTest.java
+++ infobip-typescript-generator-extension-annotation-processor-showcase/src/test/java/com/infobip/typescript/showcase/hierarchy/HierarchyTypeScriptFileGeneratorTest.java
@@ -18,79 +18,71 @@ class HierarchyTypeScriptFileGeneratorTest {
                              .collect(Collectors.joining(System.lineSeparator()));
 
         // then
-        then(actual).isEqualTo("/* tslint:disable */\n" +
-                               "/* eslint-disable */\n" +
-                               "import 'reflect-metadata';\n" +
-                               "import { Type } from 'class-transformer';\n" +
-                               "import { IsDefined, IsNotEmpty } from 'class-validator';\n" +
-                               "import { CommonValidationMessages } from './CommonValidationMessages';\n" +
-                               "\n" +
-                               "export enum Channel {\n" +
-                               "    SMS = 'SMS',\n" +
-                               "}\n" +
-                               "\n" +
-                               "export enum Direction {\n" +
-                               "    INBOUND = 'INBOUND',\n" +
-                               "    OUTBOUND = 'OUTBOUND',\n" +
-                               "}\n" +
-                               "\n" +
-                               "export enum CommonContentType {\n" +
-                               "    TEXT = 'TEXT',\n" +
-                               "}\n" +
-                               "\n" +
-                               "export interface InboundMessage extends Message {\n" +
-                               "}\n" +
-                               "\n" +
-                               "export interface Message {\n" +
-                               "    channel: Channel;\n" +
-                               "    direction: Direction;\n" +
-                               "}\n" +
-                               "\n" +
-                               "export interface OutboundMessage extends Message {\n" +
-                               "}\n" +
-                               "\n" +
-                               "export interface CommonContent extends Content<CommonContentType> {\n" +
-                               "    type: CommonContentType;\n" +
-                               "}\n" +
-                               "\n" +
-                               "export interface Content<T> {\n" +
-                               "    type: T;\n" +
-                               "}\n" +
-                               "\n" +
-                               "export interface ContentType {\n" +
-                               "}\n" +
-                               "\n" +
-                               "export class TextContent implements CommonContent {\n" +
-                               "    readonly type: CommonContentType = CommonContentType.TEXT;\n" +
-                               "    @IsDefined({ message: CommonValidationMessages.IsDefined })\n" +
-                               "    @IsNotEmpty({ message: CommonValidationMessages.IsNotEmpty })\n" +
-                               "    text: string;\n" +
-                               "}\n" +
-                               "\n" +
-                               "export class InboundSmsMessage implements InboundMessage {\n" +
-                               "    readonly channel: Channel = Channel.SMS;\n" +
-                               "    direction: Direction;\n" +
-                               "    @Type(() => Object, {\n" +
-                               "        discriminator: {\n" +
-                               "            property: 'type', subTypes: [\n" +
-                               "                { value: TextContent, name: CommonContentType.TEXT }\n" +
-                               "            ]\n" +
-                               "        }\n" +
-                               "    })\n" +
-                               "    content: CommonContent;\n" +
-                               "}\n" +
-                               "\n" +
-                               "export class OutboundSmsMessage implements OutboundMessage {\n" +
-                               "    readonly channel: Channel = Channel.SMS;\n" +
-                               "    direction: Direction;\n" +
-                               "    @Type(() => Object, {\n" +
-                               "        discriminator: {\n" +
-                               "            property: 'type', subTypes: [\n" +
-                               "                { value: TextContent, name: CommonContentType.TEXT }\n" +
-                               "            ]\n" +
-                               "        }\n" +
-                               "    })\n" +
-                               "    content: CommonContent;\n" +
-                               "}");
+        then(actual).startsWith("/* tslint:disable */\n" +
+                                "/* eslint-disable */\n" +
+                                "import 'reflect-metadata';\n" +
+                                "import { Type } from 'class-transformer';\n" +
+                                "import { IsDefined, IsNotEmpty } from 'class-validator';\n" +
+                                "import { CommonValidationMessages } from './CommonValidationMessages';\n" +
+                                "\n" +
+                                "export enum Channel {\n" +
+                                "    SMS = 'SMS',\n" +
+                                "}\n" +
+                                "\n" +
+                                "export enum Direction {\n" +
+                                "    INBOUND = 'INBOUND',\n" +
+                                "    OUTBOUND = 'OUTBOUND',\n" +
+                                "}\n" +
+                                "\n" +
+                                "export enum CommonContentType {\n" +
+                                "    TEXT = 'TEXT',\n" +
+                                "}\n" +
+                                "\n" +
+                                "export interface InboundMessage extends Message {\n" +
+                                "}\n" +
+                                "\n" +
+                                "export interface Message {")
+                    .contains("}\n" +
+                              "\n" +
+                              "export interface OutboundMessage extends Message {\n" +
+                              "}\n" +
+                              "\n" +
+                              "export interface CommonContent extends Content<CommonContentType> {\n" +
+                              "    type: CommonContentType;\n" +
+                              "}\n" +
+                              "\n" +
+                              "export interface Content<T> {\n" +
+                              "    type: T;\n" +
+                              "}\n" +
+                              "\n" +
+                              "export interface ContentType {\n" +
+                              "}\n" +
+                              "\n" +
+                              "export class TextContent implements CommonContent {\n" +
+                              "    readonly type: CommonContentType = CommonContentType.TEXT;\n" +
+                              "    @IsDefined({ message: CommonValidationMessages.IsDefined })\n" +
+                              "    @IsNotEmpty({ message: CommonValidationMessages.IsNotEmpty })\n" +
+                              "    text: string;\n" +
+                              "}")
+                    .contains("@Type(() => Object, {\n" +
+                              "        discriminator: {\n" +
+                              "            property: 'type', subTypes: [\n" +
+                              "                { value: TextContent, name: CommonContentType.TEXT }\n" +
+                              "            ]\n" +
+                              "        }\n" +
+                              "    })\n" +
+                              "    content: CommonContent;")
+                    .endsWith("export class OutboundSmsMessage implements OutboundMessage {\n" +
+                              "    readonly channel: Channel = Channel.SMS;\n" +
+                              "    direction: Direction;\n" +
+                              "    @Type(() => Object, {\n" +
+                              "        discriminator: {\n" +
+                              "            property: 'type', subTypes: [\n" +
+                              "                { value: TextContent, name: CommonContentType.TEXT }\n" +
+                              "            ]\n" +
+                              "        }\n" +
+                              "    })\n" +
+                              "    content: CommonContent;\n" +
+                              "}");
     }
 }

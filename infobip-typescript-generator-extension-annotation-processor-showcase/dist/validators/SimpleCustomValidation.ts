8a458d7502cbe3a761bdfed14121a8ffda5b84c4
msertic <marko.sertic@infobip.com>
2021-07-21 10:40:05 +0200

    WIP simpler validator showcase

--- infobip-typescript-generator-extension-annotation-processor-showcase/dist/validators/SimpleCustomValidation.ts
+++ infobip-typescript-generator-extension-annotation-processor-showcase/dist/validators/SimpleCustomValidation.ts
@@ -1,18 +1,16 @@
 import { registerDecorator, ValidationOptions, ValidationArguments } from 'class-validator';
 
-export function SimpleCustomValidation(property: string, validationOptions?: ValidationOptions) {
+export function SimpleCustomValidation(validationOptions?: ValidationOptions) {
     return function (object: Object, propertyName: string) {
         registerDecorator({
             name: 'simpleCustomValidator',
             target: object.constructor,
             propertyName: propertyName,
-            constraints: [property],
+            constraints: [],
             options: validationOptions,
             validator: {
-                validate(value: any, args: ValidationArguments) {
-                    const [relatedPropertyName] = args.constraints;
-                    const relatedValue = (args.object as any)[relatedPropertyName];
-                    return typeof value === 'string' && typeof relatedValue === 'string' && value.length > relatedValue.length; // you can return a Promise<boolean> here as well, if you want to make async validation
+                validate(value: any, args: ValidationArguments) {;
+                    return typeof value === 'string' && value.length > 100;
                 },
             },
         });

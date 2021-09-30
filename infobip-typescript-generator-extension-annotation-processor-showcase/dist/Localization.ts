dc22e9fc17efa9356cb9f3945d9c3c25a46fc350
Marko Sertic <msertic@IB-MSERTIC-M.local>
2021-09-30 10:38:09 +0200

    New Localization

--- infobip-typescript-generator-extension-annotation-processor-showcase/dist/Localization.ts
+++ infobip-typescript-generator-extension-annotation-processor-showcase/dist/Localization.ts
@@ -1,9 +1,9 @@
 export function localize(message, object: object = {}) { return format(message, object)};
 
-function format(string: string, arg: object = {}) {
+function format(text: string, arg: object = {}) {
     return Object
         .keys(arg)
         .reduce((result: string, current: string) => {
             return result.replace('{' + current + '}', (arg as any)[current]);
-        }, string);
+        }, text);
 }
